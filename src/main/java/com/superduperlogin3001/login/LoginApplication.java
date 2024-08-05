9ad42d7a4eb5d9e18be7d7b3ef32dafb29152a17
cyberpropper <osankin2002@mail.ru>
2024-08-05 22:02:55 +0700

    1

---
+++ src/main/java/com/superduperlogin3001/login/LoginApplication.java
@@ -0,0 +1,13 @@
+package com.superduperlogin3001.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+
+}
